3537df63f5b5715b178164fb40ba31301c40797a
Viruzzz <mvt3am@yahoo.com>
2021-02-09 11:11:18 +0200

    New contest has been added.

--- contest.php
+++ contest.php
@@ -35,120 +35,65 @@
 					<div class="col-md-12">
 						<div class="row row-bottom-padded-sm animate-box">
 							<div class="col-md-3">
-								<h3 class="fh5co-section-heading"><span class="fh5co-number">N<sup>o</sup> 1</span> Collect the words</h3>
+								<h3 class="fh5co-section-heading"><span class="fh5co-number">N<sup>o</sup> 2</span> Be Active</h3>
 							</div>
 							<div class="col-md-9">
 								<p class="fh5co-lead"></p>
 								<p>
-								<div class="con_end">Contest is ended</div>
 								<h3>Description</h3>
-								It is very simple contest, stay and watch on my <a href="http://twitch.com/mvt3am">mvt3am</a> channel, wait and see the bot word and write the word on chat.<br />
-								Who collect the most of word can win. see <a href="#rules">rules</a>.
-								<br />
-								You must to collect all the words from 1.10.2016 to 9.10.2016.
+								It is very simple contest, stay and watch on me <a href="http://twitch.com/mvt3am">mvt3am</a> channel everyday.<br>
+                                More active more chance to win!<br>
+                                Everyday I post on twitter the percent of the win rate +-0/3%* and will be selected the winners in transparent mode.
 								<h3 id="rules">Rules</h3>
-								You must to follow me if you do want to join.<br />
-								You have 20 minutes before the word expired, so you must to be fast and not fast. Only be attention.<br />
-								You can't see the word in video replay, don't try to cheat it.
-								If are 2-4 followers all 9 words, all goes to spin round.
-								<h3>Buy</h3>
-								Yes you can buy with your points the word if you miss it, deduct from your all points.
-								<ul>
-									<li>40 Points</li>
-									<li>30 Points - subscription on Youtube <a href="https://www.youtube.com/channel/UCzPfw3b7L4oVr9C4BykM-8w">mvt3am</a> channel</li>
-								</ul>
+								You must to follow me if you do want to join at contest.<br>
+                                You must to be active almost 2 weeks or more before to win.<br>
 								<h3>Prize</h3>
-								Moderator access<br />
-								One title for you, your choice what you do want.<br />
+								One from 5 ViPs<br>
+                                <h3>Note</h3>
+                                * reprezent the active viewers on my stream.
 								</p>
 							</div>
 						</div>
-<!--
-						<div class="row row-bottom-padded-sm animate-box">
-							<div class="col-md-12">
-								<figure class="fh5co-bio-photo"><img src="images/img.jpg" class="img-responsive" alt=""></figure>
-							</div>
-						</div>
--->
-<!--
-						<div class="row animate-box row-bottom-padded-sm">
-							<div class="col-md-3">
-								<h3 class="fh5co-section-heading"><span class="fh5co-number">N<sup>o</sup> 2</span> Games</h3>
-							</div>
-							<div class="col-md-9">
-								
-								<p class="fh5co-lead">In some cases depend by viewers what I play in weekend and in the rest of days I play as usual.</p>
-								<p>
-								The Sims 4 (lots/households/mods/custom items <a href="../sims/">here</a>)<br />
-								Euro Truck Simulator 2<br />
-								RimWorld<br />
-								Kerbal Space Program (<a href="ksp_mods">mods</a>)<br />
-								Minecraft<br />
-								</p>
-							</div>
-						</div>
-						
-
-						<div class="row animate-box row-bottom-padded-sm">
-							<div class="col-md-3">
-								<h3 class="fh5co-section-heading"><span class="fh5co-number">N<sup>o</sup> 3</span> Points and Reward</h3>
-							</div>
-							<div class="col-md-9">
-								
-								<p class="fh5co-lead">Watch me on my twitch channel and earn points.</p>
-								<p>
-								<h3>Description</h3>
-								In my channel the Yoshikobot in different time you earn points, only if you are watching when I stream.<br />
-								You start to earn points on every 30 minutes spent on my channel.<br />
-								If you are new on my channel you must to wait 5-20 minutes before to earn points.<br />
-								<br />
-								<h3>Points Bonus List</h3>
-								<ul>
-									<li>Live - 1 Point (<i>watching me</i>)</li>
-									<li>Follow - 10 Points (<i>you follow me</i>)</li>
-									<li>Active - 1 Point (<i>write on chat</i>)</li>
-									<li>Host - 3 Points (<i>use the /host command</i>)</li>
-								</ul>
-								<br />
-								<h3>Ranks</h3>
-								<ul>
-									<li>250 - Initiate</li>
-									<li>500 - Key Keeper</li>
-									<li>750 - Knight</li>
-									<li>800 - Winner of Cup (*)</li>
-									<li>1000 - First Sword</li>
-								</ul>
-								<i>The title ranks can be changed in time but not the Points.</i>
-								<br />
-								<h3>Legend</h3>
-								* You can win a reward if you have Points see the twitter if in a month, week you can earn Rewards.
-								<br />
-								<h3>FAQ</h3>
-								<b>Why I not earn points?</b><br />
-							    You not earn points if you not Follow me.
-								<br />    
-							    <b>If donated I can receive points?</b><br />
-							    Yes you receive points, ex 1&euro; = 50 Points same as for $. (can be more points with every donation)
-								<br />
-							    <b>I donated but I not receive the points</b><br />
-								Send me a message private and in short time you have it all.
-								<br />	
-								<b>If I donated 100&euro; / $ how many points I win?</b><br />
-								You not win, you earn 10.000 points and You will have a special rank only for you. The title you choice it only for you.
-								<br />
-								<b>I can buy something else with Points?</b><br />
-								Yes, as soon the our shop is complete.
-								<br />
-								<b>I donated / I watching you everytime what I earn?</b><br />
-								You earn Points and my respect.
-								<br />
-								</p>
-							</div>
-						</div>
--->
-
 					</div>
 				</div>
 			</div>
 		</div>
+        <div class="js-fh5co-waypoint fh5co-project-detail" id="fh5co-main" data-colorbg="">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="row row-bottom-padded-sm animate-box">
+                            <div class="col-md-3">
+                                <h3 class="fh5co-section-heading"><span class="fh5co-number">N<sup>o</sup> 1</span> Collect the words</h3>
+                            </div>
+                            <div class="col-md-9">
+                                <p class="fh5co-lead"></p>
+                                <p>
+                                <div class="con_end">Contest is ended</div>
+                                <h3>Description</h3>
+                                It is very simple contest, stay and watch on me <a href="http://twitch.com/mvt3am">mvt3am</a> channel, wait and see the bot word and write the word on chat.<br>
+                                Who collect the most of word can win. see <a href="#rules">rules</a>.
+                                <br>
+                                You must to collect all the words from 1.10.2016 to 9.10.2016.
+                                <h3 id="rules">Rules</h3>
+                                You must to follow me if you do want to join.<br>
+                                You have 20 minutes before the word expired, so you must to be fast and not fast. Only be attention.<br>
+                                You can't see the word in video replay, don't try to cheat it.
+                                If are 2-4 followers all 9 words, all goes to spin round.
+                                <h3>Buy</h3>
+                                Yes you can buy with your points the word if you miss it, deduct from your all points.
+                                <ul>
+                                    <li>40 Points</li>
+                                    <li>30 Points - subscription on Youtube <a href="https://www.youtube.com/channel/UCzPfw3b7L4oVr9C4BykM-8w">mvt3am</a> channel</li>
+                                </ul>
+                                <h3>Prize</h3>
+                                Moderator access<br>
+                                One title for you, your choice what you do want.<br>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <?php include 'parts/footer.php';?>
