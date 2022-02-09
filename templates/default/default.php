9ffb27cc8257e22f40ea9545e922869111b1ad93
Viruzzz <mvt3am@yahoo.com>
2022-02-09 22:26:51 +0200

    typo href at header and footer links

--- templates/default/default.php
+++ templates/default/default.php
@@ -6,13 +6,13 @@
     <meta name="description" content="<?php echo $description;?>">
     <meta name="keywords" content="<?php echo $keywords;?>">
     <meta name="author" content="<?php echo $copyright;?>">
-    <link rel="shortcut icon" href="<?php echo BASE_HREF;?>/templates/<?php echo $theme;?>/images/favicon.png">
+    <link rel="shortcut icon" href="<?php echo BASE_HREF;?>templates/<?php echo $theme;?>/images/favicon.png">
     <link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,400italic,700italic,700' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="<?php echo BASE_HREF;?>/templates/<?php echo $theme;?>/css/animate.css">
-    <link rel="stylesheet" href="<?php echo BASE_HREF;?>/templates/<?php echo $theme;?>/css/flexslider.css">
-    <link rel="stylesheet" href="<?php echo BASE_HREF;?>/templates/<?php echo $theme;?>/css/icomoon.css">
-    <link rel="stylesheet" id="theme-switch" href="<?php echo BASE_HREF;?>/templates/<?php echo $theme;?>/css/style.css">
+    <link rel="stylesheet" href="<?php echo BASE_HREF;?>templates/<?php echo $theme;?>/css/animate.css">
+    <link rel="stylesheet" href="<?php echo BASE_HREF;?>templates/<?php echo $theme;?>/css/flexslider.css">
+    <link rel="stylesheet" href="<?php echo BASE_HREF;?>templates/<?php echo $theme;?>/css/icomoon.css">
+    <link rel="stylesheet" id="theme-switch" href="<?php echo BASE_HREF;?>templates/<?php echo $theme;?>/css/style.css">
         <?php
         if (isset($_GET['od'])) {{
             $od = "modules/default/" . $_GET['od'] . ".php";
@@ -37,17 +37,17 @@
             </div>
         </footer>
     </div>
-    <script src="<?php echo BASE_HREF;?>/templates/default/js/jquery.min.js"></script>
-    <script src="<?php echo BASE_HREF;?>/templates/default/js/jquery.cookie.js"></script>
-    <script src="<?php echo BASE_HREF;?>/templates/default/js/jquery.easing.1.3.js"></script>
+    <script src="<?php echo BASE_HREF;?>templates/default/js/jquery.min.js"></script>
+    <script src="<?php echo BASE_HREF;?>templates/default/js/jquery.cookie.js"></script>
+    <script src="<?php echo BASE_HREF;?>templates/default/js/jquery.easing.1.3.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
-    <script src="<?php echo BASE_HREF;?>/templates/default/js/jquery.waypoints.min.js"></script>
-    <script src="<?php echo BASE_HREF;?>/templates/default/js/jquery.flexslider-min.js"></script>
-    <script src="<?php echo BASE_HREF;?>/templates/default/js/viewport-units-buggyfill.js"></script>
-    <script src="<?php echo BASE_HREF;?>/templates/default/js/main.js"></script>
-    <script src="<?php echo BASE_HREF;?>/templates/default/js/modernizr-2.6.2.min.js"></script>
-    <script src="<?php echo BASE_HREF;?>/templates/default/js/respond.min.js"></script>
+    <script src="<?php echo BASE_HREF;?>templates/default/js/jquery.waypoints.min.js"></script>
+    <script src="<?php echo BASE_HREF;?>templates/default/js/jquery.flexslider-min.js"></script>
+    <script src="<?php echo BASE_HREF;?>templates/default/js/viewport-units-buggyfill.js"></script>
+    <script src="<?php echo BASE_HREF;?>templates/default/js/main.js"></script>
+    <script src="<?php echo BASE_HREF;?>templates/default/js/modernizr-2.6.2.min.js"></script>
+    <script src="<?php echo BASE_HREF;?>templates/default/js/respond.min.js"></script>
     <script src="https://getbootstrap.com/docs/5.1/assets/js/docs.min.js"></script>
     <script src="https://code.iconify.design/2/2.1.2/iconify.min.js"></script>
 </body>
