1b4482052ec4be364c0d1763be594f0967bd4240
Viruzzz <mvt3am@yahoo.com>
2024-02-02 18:00:24 +0200

    Added: Creation Zone page

---
+++ modules/default/creation_zone.php
@@ -0,0 +1,68 @@
+<title>Creation Zone | <?php echo $siteName;?></title>
+</head>
+<body>
+<div class="fh5co-loader"></div>
+<div id="fh5co-page">
+    <nav id="fh5co-nav" role="navigation">
+        <ul>
+            <li class="animate-box"><a href="<?php echo BASE_HREF;?>home" class="transition">Home</a></li>
+            <li class="animate-box"><a href="<?php echo BASE_HREF;?>stream" class="transition">Stream</a></li>
+            <li class="animate-box"><a href="<?php echo BASE_HREF;?>contest" class="transition">Contest</a></li>
+            <li class="animate-box fh5co-active"><a href="<?php echo BASE_HREF;?>creation_zone" class="transition">Creation Zone</a></li>
+            <li class="animate-box"><a href="<?php echo BASE_HREF;?>donation_box" class="transition">Donation Box</a></li>
+            <li class="animate-box"><a href="<?php echo BASE_HREF;?>about" class="transition">About</a></li>
+        </ul>
+    </nav>
+    <header id="fh5co-header" role="banner" class="fh5co-project js-fh5co-waypoint no-border" data-colorbg="#222222" data-next="yes">
+        <div class="container">
+            <div class="fh5co-text-wrap animate-box">
+                <div class="fh5co-intro-text">
+                    <h1>Creation Zone</h1>
+                </div>
+            </div>
+        </div>
+        <div class="btn-next animate-box fh5co-learn-more">
+            <a href="#" class="scroll-btn"><span>See the detail</span><i class="fa-solid fa-chevron-down"></i></a>
+        </div>
+    </header>
+    <div class="js-fh5co-waypoint fh5co-project-detail" id="fh5co-main" data-colorbg="">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="row row-bottom-padded-sm animate-box">
+                        <div class="col-md-3">
+                            <h3 class="fh5co-section-heading"><span class="fh5co-number">N<sup>o</sup> 1</span> About</h3>
+                        </div>
+                        <div class="col-md-9">
+                            What is the Creation Zone?!<br>
+                            The Creation Zone it is for helping me to upgrade my software for 3D modeling and other costs as software licencing etc.<br>
+                            <i>Reward:</i> For every &euro; spend you earn some reward on our discord server and more.
+                            <br>
+
+                        </div>
+                    </div>
+                    <div class="row animate-box row-bottom-padded-sm">
+                        <div class="col-md-3">
+                            <h3 class="fh5co-section-heading"><span class="fh5co-number">N<sup>o</sup> 2</span> <i class="fa-solid fa-euro-sign"></i></h3>
+                        </div>
+                        <div class="col-md-9">
+                            <p>
+                                You can donate directly to my PayPal address: <a href="https://www.paypal.com/donate/?hosted_button_id=AGPSVXNLNTTB4"><i class="fa-brands fa-paypal fa-beat" style="color: #74C0FC;"></i></a>
+                                <br>
+                                Or scan the QR code <i class="fa-solid fa-hand-point-down" style="color: #800000;"></i><br>
+                                <img src="<?php echo BASE_HREF;?>assets/misc/QR_Code_mv_cz.png"/>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row animate-box row-bottom-padded-sm">
+                        <div class="col-md-3">
+                            <h3 class="fh5co-section-heading"><span class="fh5co-number">N<sup>o</sup> 3</span> <i class="fa-solid fa-list"></i></h3>
+                        </div>
+                        <div class="col-md-9">
+                            <p>Members who has donated (Is an optional choice, published only with your wish).</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
